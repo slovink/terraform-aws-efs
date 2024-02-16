9f44abc83606180b719663a39b4e790de472e54b
vinodyadav95 <thevinodyadav95@gmail.com>
2024-02-16 16:51:33 +0530

    inital commot

---
+++ _example/example.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  environment        = "test"
+  label_order        = ["name", "environment"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
+}
+
+module "vpc" {
+  source = "git@github.com:slovink/terraform-aws-vpc.git?ref=vinod"
+  #  version     = "2.0.0"
+  name        = "vpc"
+  environment = local.environment
+  label_order = local.label_order
+  cidr_block  = "172.16.0.0/16"
+}
+
+#tfsec:ignore:aws-ec2-no-excessive-port-access # Ingnored because these are basic examples, it can be changed via varibales as per requirement.
+#tfsec:ignore:aws-ec2-no-public-ingress-acl # Ingnored because these are basic examples, it can be changed via varibales as per requirement.
+module "subnets" {
+  source = "git@github.com:slovink/terraform-aws-subnet.git?ref=test"
+  #  version            = "2.0.1"
+  name               = "subnet"
+  environment        = local.environment
+  label_order        = local.label_order
+  availability_zones = local.availability_zones
+  vpc_id             = module.vpc.id
+  cidr_block         = module.vpc.vpc_cidr_block
+  type               = "public"
+  igw_id             = module.vpc.igw_id
+  ipv6_cidr_block    = module.vpc.ipv6_cidr_block
+}
+
+module "efs" {
+  source                    = "./.."
+  name                      = "efs"
+  environment               = "test"
+  creation_token            = "changeme"
+  availability_zones        = local.availability_zones
+  vpc_id                    = module.vpc.id
+  subnets                   = module.subnets.public_subnet_id
+  security_groups           = [module.vpc.vpc_default_security_group_id]
+  efs_backup_policy_enabled = true
+  allow_cidr                = [module.vpc.vpc_cidr_block] #vpc_cidr
+  replication_enabled       = true
+  replication_configuration_destination = {
+    region                 = "eu-west-1"
+    availability_zone_name = ["eu-west-1a", "eu-west-1b"]
+  }
+}
